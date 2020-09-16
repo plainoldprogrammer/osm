ba8d5755cfdbde57ad8339f54e8ac7bf335ae75a
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-09-16 00:46:28 -0500

    renaming the main Form

---
+++ WindowsFormsApp/Form.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using ConsoleApp;
+
+namespace WindowsFormsApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            DatabaseAccess databaseAccess = new DatabaseAccess();
+            textBox1.Text = databaseAccess.GetSnippetsTitles();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- WindowsFormsApp/Program.cs
+++ WindowsFormsApp/Program.cs
@@ -6,18 +6,18 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp
 {
-	static class Program
-	{
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			Application.SetHighDpiMode(HighDpiMode.SystemAware);
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
-		}
-	}
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form());
+        }
+    }
 }
