44cdd8ce653278892a95563f42f43c7a4ff4b4f0
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-10-18 23:33:17 -0500

    aligning the categories label

--- WindowsFormsApp/Form.Designer.cs
+++ WindowsFormsApp/Form.Designer.cs
@@ -65,7 +65,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Location = new System.Drawing.Point(8, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 15);
             this.label1.TabIndex = 3;
