5860315ebe47936fe7cef1be0452222508239444
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-10-18 23:29:21 -0500

    adding the categories label

--- WindowsFormsApp/Form.Designer.cs
+++ WindowsFormsApp/Form.Designer.cs
@@ -31,6 +31,7 @@
             this.snippetsCategories = new System.Windows.Forms.TextBox();
             this.snippetsTitles = new System.Windows.Forms.TextBox();
             this.snippetContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // snippetsCategories
@@ -59,11 +60,21 @@
             this.snippetContent.Size = new System.Drawing.Size(514, 474);
             this.snippetContent.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Categories";
+            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1040, 585);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.snippetContent);
             this.Controls.Add(this.snippetsTitles);
             this.Controls.Add(this.snippetsCategories);
@@ -79,5 +90,6 @@
         private System.Windows.Forms.TextBox snippetsCategories;
         private System.Windows.Forms.TextBox snippetsTitles;
         private System.Windows.Forms.TextBox snippetContent;
+        private System.Windows.Forms.Label label1;
     }
 }
