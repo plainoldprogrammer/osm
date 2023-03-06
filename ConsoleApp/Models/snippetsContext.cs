7e16a18e5fc753a1d005a40fba341415a9e7cf26
plainoldprogrammer <plainoldprogrammer@gmail.com>
2023-03-05 22:13:09 -0600

    changing the db path for development

--- ConsoleApp/Models/snippetsContext.cs
+++ ConsoleApp/Models/snippetsContext.cs
@@ -23,7 +23,7 @@ namespace ConsoleApp.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlite("DataSource=C:\\Plainoldprogrammer\\osm\\db\\snippets.db");
+                optionsBuilder.UseSqlite("DataSource=C:\\Plainoldprogrammer_dev\\osm\\db\\snippets.db");
             }
         }
 
