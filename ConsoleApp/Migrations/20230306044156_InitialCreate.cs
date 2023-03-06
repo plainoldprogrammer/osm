cea475ab41612214d6cb3e6ba61577a3eddde1f8
plainoldprogrammer <plainoldprogrammer@gmail.com>
2023-03-06 00:02:05 -0600

    adding the first migration

---
+++ ConsoleApp/Migrations/20230306044156_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false),
+                    category = table.Column<string>(type: "TEXT", nullable: true),
+                    datetime = table.Column<byte[]>(type: "DATETIME", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "snippets",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false),
+                    title = table.Column<string>(type: "TEXT", nullable: true),
+                    snippet = table.Column<string>(type: "TEXT", nullable: true),
+                    category = table.Column<string>(type: "TEXT", nullable: true),
+                    datetime = table.Column<byte[]>(type: "DATETIME", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_snippets", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "snippets");
+        }
+    }
+}
