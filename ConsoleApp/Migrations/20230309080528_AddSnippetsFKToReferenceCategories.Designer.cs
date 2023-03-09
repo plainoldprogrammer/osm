b66b3ae9c1169327510f6596608d4e3431c9018f
plainoldprogrammer <plainoldprogrammer@gmail.com>
2023-03-09 02:07:00 -0600

    creating a migration to add the snippets fk to reference to categories

--- ConsoleApp/Migrations/20230309080528_AddSnippetsFKToReferenceCategories.Designer.cs
+++ ConsoleApp/Migrations/20230309080528_AddSnippetsFKToReferenceCategories.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ConsoleApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ConsoleApp.Migrations
 {
     [DbContext(typeof(SnippetsContext))]
-    partial class SnippetsContextModelSnapshot : ModelSnapshot
+    [Migration("20230309080528_AddSnippetsFKToReferenceCategories")]
+    partial class AddSnippetsFKToReferenceCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
---
+++ ConsoleApp/Migrations/20230309080528_AddSnippetsFKToReferenceCategories.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp.Migrations
+{
+    public partial class AddSnippetsFKToReferenceCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "category",
+                table: "snippets");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                table: "snippets",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "category_id",
+                table: "snippets",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_snippets_category_id",
+                table: "snippets",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_snippets_id",
+                table: "snippets",
+                column: "id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_snippets_categories_category_id",
+                table: "snippets",
+                column: "category_id",
+                principalTable: "categories",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_snippets_categories_category_id",
+                table: "snippets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_snippets_category_id",
+                table: "snippets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_snippets_id",
+                table: "snippets");
+
+            migrationBuilder.DropColumn(
+                name: "category_id",
+                table: "snippets");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                table: "snippets",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "category",
+                table: "snippets",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
