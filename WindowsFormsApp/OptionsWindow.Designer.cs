34bb2de7e67613261422872d2ca492238bdf93dd
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2023-03-17 04:50:42 -0500

    adding controls to the options window

--- WindowsFormsApp/OptionsWindow.Designer.cs
+++ WindowsFormsApp/OptionsWindow.Designer.cs
@@ -28,18 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            comboBox1 = new System.Windows.Forms.ComboBox();
+            label3 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            comboBox2 = new System.Windows.Forms.ComboBox();
+            checkBox1 = new System.Windows.Forms.CheckBox();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 23);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(46, 15);
+            label1.TabIndex = 0;
+            label1.Text = "DB File:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(64, 20);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(296, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(366, 20);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(52, 23);
+            button1.TabIndex = 2;
+            button1.Text = "...";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(12, 71);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(46, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Theme:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new System.Drawing.Point(12, 89);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(121, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(12, 128);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(34, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Font:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(12, 146);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(188, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(230, 128);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(30, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Size:";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new System.Drawing.Point(230, 146);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new System.Drawing.Size(71, 23);
+            comboBox2.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(322, 148);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(86, 19);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "Word Wrap";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(256, 281);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(75, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Ok";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(342, 281);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(75, 23);
+            button3.TabIndex = 11;
+            button3.Text = "Cancel";
+            button3.UseVisualStyleBackColor = true;
             // 
             // OptionsWindow
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(430, 329);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(checkBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Name = "OptionsWindow";
             Text = "OptionsWindow";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
