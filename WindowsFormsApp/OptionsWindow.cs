f4231cbe405054b40428f05c78071b890d26ff15
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2023-03-17 05:48:19 -0500

    showing all system installed fonts in the options window

--- WindowsFormsApp/OptionsWindow.cs
+++ WindowsFormsApp/OptionsWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace Osm
         {
             InitializeComponent();
             _mainWindow = mainWindow;
+            GetAllSystemInstalledFonts();
         }
 
         private void buttonOk_Click(object sender, EventArgs e)
@@ -36,6 +38,17 @@ namespace Osm
         {
             _mainWindow.Enabled = true;
             this.Hide();
+        }
+
+        private void GetAllSystemInstalledFonts()
+        {
+            using (InstalledFontCollection fontCollection = new InstalledFontCollection())
+            {
+                foreach (FontFamily fontFamily in fontCollection.Families)
+                {
+                    this.comboBoxFont.Items.Add(fontFamily.Name);
+                }
+            }
         }
     }
 }
