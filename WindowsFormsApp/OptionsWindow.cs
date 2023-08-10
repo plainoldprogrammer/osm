5187b855792d731b75ea68e4e87b3cbca1821f11
plainoldprogrammer <plainoldprogrammer@gmail.com>
2023-08-10 13:23:49 -0500

    enabling to close the options window

--- WindowsFormsApp/OptionsWindow.cs
+++ WindowsFormsApp/OptionsWindow.cs
@@ -19,109 +19,117 @@ using WindowsFormsApp;
 
 namespace Osm
 {
-    public partial class OptionsWindow : Form
-    {
-        private MainWindow _mainWindow;
-        private DatabaseAccess _databaseAccess;
+	public partial class OptionsWindow : Form
+	{
+		private MainWindow _mainWindow;
+		private DatabaseAccess _databaseAccess;
 
-        public OptionsWindow(MainWindow mainWindow, DatabaseAccess databaseAccess)
-        {
-            InitializeComponent();
-            _mainWindow = mainWindow;
-            _databaseAccess = databaseAccess;
-            this.textBoxDbFile.Text = _databaseAccess.GetDatabasePath();
-            GetAllSystemInstalledFonts();
-            this.AcceptButton = this.buttonOk;
-            this.CancelButton = this.buttonCancel;
+		public OptionsWindow(MainWindow mainWindow, DatabaseAccess databaseAccess)
+		{
+			InitializeComponent();
+			_mainWindow = mainWindow;
+			_databaseAccess = databaseAccess;
+			this.textBoxDbFile.Text = _databaseAccess.GetDatabasePath();
+			GetAllSystemInstalledFonts();
+			this.AcceptButton = this.buttonOk;
+			this.CancelButton = this.buttonCancel;
 
-            var applicationThemeOnConfig = Properties.Settings.Default["ApplicationTheme"];
+			var applicationThemeOnConfig = Properties.Settings.Default["ApplicationTheme"];
 
-            if (applicationThemeOnConfig.ToString().ToLower().Equals("") || applicationThemeOnConfig.ToString().ToLower().Equals("light"))
-            {
-                this.comboBoxTheme.SelectedIndex = 0;
-            }
-            else if (applicationThemeOnConfig.ToString().ToLower().Equals("dark"))
-            {
-                this.comboBoxTheme.SelectedIndex = 1;
-            }
+			if (applicationThemeOnConfig.ToString().ToLower().Equals("") || applicationThemeOnConfig.ToString().ToLower().Equals("light"))
+			{
+				this.comboBoxTheme.SelectedIndex = 0;
+			}
+			else if (applicationThemeOnConfig.ToString().ToLower().Equals("dark"))
+			{
+				this.comboBoxTheme.SelectedIndex = 1;
+			}
 
-            float fontSize = _mainWindow.GetRichTextBoxSnippetContentFontSize();
-            int indexFontSize = this.comboBoxFontSize.Items.IndexOf(((int)fontSize).ToString());
-            this.comboBoxFontSize.SelectedIndex = indexFontSize;
+			float fontSize = _mainWindow.GetRichTextBoxSnippetContentFontSize();
+			int indexFontSize = this.comboBoxFontSize.Items.IndexOf(((int)fontSize).ToString());
+			this.comboBoxFontSize.SelectedIndex = indexFontSize;
 
-            Font font = _mainWindow.GetRichTextBoxSnippetContentFont();
-            int indexFont = this.comboBoxFont.Items.IndexOf(font.FontFamily.Name);
-            this.comboBoxFont.SelectedIndex = indexFont;
+			Font font = _mainWindow.GetRichTextBoxSnippetContentFont();
+			int indexFont = this.comboBoxFont.Items.IndexOf(font.FontFamily.Name);
+			this.comboBoxFont.SelectedIndex = indexFont;
 
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+			this.FormBorderStyle = FormBorderStyle.FixedDialog;
 		}
 
-        private void buttonOk_Click(object sender, EventArgs e)
-        {
-            _mainWindow.Enabled = true;
+		private void buttonOk_Click(object sender, EventArgs e)
+		{
+			_mainWindow.Enabled = true;
 
-            if (this.comboBoxTheme.SelectedItem == "Light")
-            {
-                _mainWindow.SetLightTheme();
-                
-                Properties.Settings.Default["ApplicationTheme"] = "Light";
-                Properties.Settings.Default.Save();
-            }
-            else if (this.comboBoxTheme.SelectedItem == "Dark")
-            {
-                _mainWindow.SetDarkTheme();
+			if (this.comboBoxTheme.SelectedItem == "Light")
+			{
+				_mainWindow.SetLightTheme();
 
-                Properties.Settings.Default["ApplicationTheme"] = "Dark";
-                Properties.Settings.Default.Save();
-            }
+				Properties.Settings.Default["ApplicationTheme"] = "Light";
+				Properties.Settings.Default.Save();
+			}
+			else if (this.comboBoxTheme.SelectedItem == "Dark")
+			{
+				_mainWindow.SetDarkTheme();
 
-            if (this.comboBoxFont.SelectedItem is not null)
-            {
-                String selectedFont = this.comboBoxFont.SelectedItem.ToString();
-                _mainWindow.SetFont(selectedFont);
-            }
+				Properties.Settings.Default["ApplicationTheme"] = "Dark";
+				Properties.Settings.Default.Save();
+			}
 
-            if (this.comboBoxFontSize.SelectedItem is not null)
-            {
-                int fontSize = 9;
-                Int32.TryParse(this.comboBoxFontSize.SelectedItem.ToString(), out fontSize);
-                _mainWindow.SetFontSize(fontSize);
-            }
+			if (this.comboBoxFont.SelectedItem is not null)
+			{
+				String selectedFont = this.comboBoxFont.SelectedItem.ToString();
+				_mainWindow.SetFont(selectedFont);
+			}
 
-            this.Hide();
-        }
+			if (this.comboBoxFontSize.SelectedItem is not null)
+			{
+				int fontSize = 9;
+				Int32.TryParse(this.comboBoxFontSize.SelectedItem.ToString(), out fontSize);
+				_mainWindow.SetFontSize(fontSize);
+			}
 
-        private void buttonCancel_Click(object sender, EventArgs e)
-        {
-            _mainWindow.Enabled = true;
-            this.Hide();
-        }
+			this.Hide();
+		}
 
-        private void GetAllSystemInstalledFonts()
-        {
-            using (InstalledFontCollection fontCollection = new InstalledFontCollection())
-            {
-                foreach (FontFamily fontFamily in fontCollection.Families)
-                {
-                    this.comboBoxFont.Items.Add(fontFamily.Name);
-                }
-            }
-        }
+		private void buttonCancel_Click(object sender, EventArgs e)
+		{
+			_mainWindow.Enabled = true;
+			this.Hide();
+		}
 
-        private void buttonSelectDbFile_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+		private void GetAllSystemInstalledFonts()
+		{
+			using (InstalledFontCollection fontCollection = new InstalledFontCollection())
+			{
+				foreach (FontFamily fontFamily in fontCollection.Families)
+				{
+					this.comboBoxFont.Items.Add(fontFamily.Name);
+				}
+			}
+		}
 
-            string filePath = this.textBoxDbFile.Text; ;
-            string directory = filePath.Substring(0, filePath.LastIndexOf("\\"));
-            openFileDialog.InitialDirectory = directory;
+		private void buttonSelectDbFile_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
 
-            openFileDialog.ShowDialog();
-        }
+			string filePath = this.textBoxDbFile.Text; ;
+			string directory = filePath.Substring(0, filePath.LastIndexOf("\\"));
+			openFileDialog.InitialDirectory = directory;
 
-        private void checkBoxWordWrap_CheckedChanged(object sender, EventArgs e)
-        {
+			openFileDialog.ShowDialog();
+		}
 
-        }
-    }
+		private void checkBoxWordWrap_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void OptionsWindow_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			this.Hide();
+			e.Cancel = true;
+
+			_mainWindow.Enabled = true;
+		}
+	}
 }
