78f240e5e271742782bc24b3dcb677793070acbc
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2025-01-04 01:11:43 -0600

    removing a this reference

--- WindowsFormsApp/MainWindow.cs
+++ WindowsFormsApp/MainWindow.cs
@@ -43,7 +43,7 @@ namespace WindowsFormsApp
             }
             */
 
-            this.InitializeComponent();
+            InitializeComponent();
             this.InitializeDatabaseAcess();
 
             this._createCategoryWindow = new CreateCategoryWindow(this, this._databaseAccess);
