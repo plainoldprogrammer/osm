e04f9875c44c32783167a7a4809d4eec8809a45a
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2025-01-05 23:51:00 -0600

    adding a this reference

--- WindowsFormsApp/CreateCategoryWindow.cs
+++ WindowsFormsApp/CreateCategoryWindow.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApp
             InitializeComponent();
 
             _mainWindow = mainWindow;
-            _databaseAccess = databaseAccess;
+            this._databaseAccess = databaseAccess;
 
             this.textBoxCategoryName.Select();
             this.AcceptButton = this.buttonOk;
