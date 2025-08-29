a994f7b0e928170d4f82af0d9b6e800fc51d0dfc
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2025-08-28 23:41:12 -0500

    adding the public modifier

--- WindowsFormsApp/Program.cs
+++ WindowsFormsApp/Program.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
