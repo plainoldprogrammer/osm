bd04fd43a1bc797a155502673fc038bde1bdf362
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2025-08-29 23:40:02 -0500

    adding the public modifier

--- WindowsFormsApp/Program.cs
+++ WindowsFormsApp/Program.cs
@@ -15,7 +15,7 @@ namespace WindowsFormsApp
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
